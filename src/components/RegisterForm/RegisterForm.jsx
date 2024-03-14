8a7125a01cff9149cf07d35b32efa20ddafe638d
VadymMurzin <prometey736@gmail.com>
2024-03-14 08:02:00 +0100

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -19,7 +19,7 @@ const validationFormSchema = yup.object().shape({
     .string()
     .required('No password provided.')
     .min(8, 'Password is too short - should be 8 chars minimum.')
-    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
+    // .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
 });
 
 export default function RegisterForm() {
